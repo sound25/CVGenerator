8c7ed387f035b4c3dd94c238d14fd1a0fa13ce7c
Soundarya <34751165+sound25@users.noreply.github.com>
2021-11-06 13:08:11 +0530

    added a model and forms to input and output
    added a model and forms to input and output

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.CharField(max_length=200)
+    phone=models.CharField(max_length=200)
+    summary=models.TextField(max_length=2000)
+    school=models.CharField(max_length=200)
+    univesity=models.CharField(max_length=200)
+    degree=models.CharField(max_length=200)
+    previous_work=models.TextField(max_length=2000)
+    skills=models.CharField(max_length=2000)
--- pdf/views.py
+++ pdf/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+def index(request):
+    if request.method=='POST':
+        name=request.POST.get('name',"")
+        email=request.POST.get('email',"")
+        phone=request.POST.get('phone',"")
+        summary=request.POST.get('summary',"")
+        skills=request.POST.get('skills',"")
+        previous_work=request.POST.get('previous_Work',"")
+        degree=request.POST.get('degree',"")
+        school=request.POST.get('school',"")
+        university=request.POST.get('university',"")
+        profile=Profile(name=name,email=email,skills=skills,phone=phone,degree=degree,univesity=university,previous_work=previous_work,summary=summary,school=school)
+        profile.save()
+    return render(request,'pdf/index.html')
+
+def resume(request,id):
+    user_profile=Profile.objects.get(pk=id)
+    return render(request,'pdf/resume.html',{'user_profile':user_profile})
